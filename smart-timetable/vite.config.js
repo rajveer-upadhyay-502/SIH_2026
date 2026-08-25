991e9a9fb494f81c36ab7d76c9e68aa6c809561c
rajveer-upadhyay-502 <urajveer7@gmail.com>
2026-08-25 17:07:02 +0530

    deployment issue resolve

--- smart-timetable/vite.config.js
+++ smart-timetable/vite.config.js
@@ -3,10 +3,5 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react({
-      include: /\.(jsx|js)$/,
-    }),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
 });
