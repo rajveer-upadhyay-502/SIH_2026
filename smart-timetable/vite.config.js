515ad62266e0358faf34964f3d2c2ac2f95160e8
rajveer-upadhyay-502 <urajveer7@gmail.com>
2026-08-25 16:53:48 +0530

    use of native plugin for js/jsx parsing

--- smart-timetable/vite.config.js
+++ smart-timetable/vite.config.js
@@ -4,7 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
-    react(),
+    react({
+      include: /\.(jsx|js)$/,
+    }),
     tailwindcss(),
   ],
 });
